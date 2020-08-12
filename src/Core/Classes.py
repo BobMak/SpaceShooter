fixed sprite position and rotation

src/Core/Classes.py
@@ -63,8 +63,8 @@ class Object:
         if self.speed[0] or self.speed[1]:
             self.position = (self.position[0]+self.speed[0], self.position[1]+self.speed[1])
             self.rect = (self.position[0]-self.rect[2]//2, self.position[1]-self.rect[3]//2, self.rect[2], self.rect[3])
-            self.image.x = self.rect[0] // 2
-            self.image.y = self.rect[1] // 2
+            self.image.x = self.rect[0] + St.window.base_x
+            self.image.y = self.rect[1] + St.window.base_y
             ang = np.arctan(self.speed[1]/self.speed[0])
             self.speed[0] += -np.sign(self.speed[0]) * 0.01 * abs(np.cos(ang))
             self.speed[1] += -np.sign(self.speed[1]) * 0.01 * abs(np.sin(ang))
@@ -72,6 +72,9 @@ class Object:
     def accelerate(self, dl, angle):
         self.speed[0] += dl * np.cos(np.deg2rad(angle - 90.0))
         self.speed[1] += dl * np.sin(np.deg2rad(angle - 90.0))
+
+    def set_rotation(self, angle):
+        self.ang = 360-angle
 
     def rotate(self, dir):
         # Ensure that look_dir is in range [0 - 360)
@@ -122,7 +125,9 @@ class Object:
             f()
 
     def draw(self):
-        # print("draw", self.__class__, self.image.x, self.image.y)
+        self.image.x = self.rect[0] - St.window.base_x
+        self.image.y = self.rect[1] - St.window.base_y
+        self.image.rotation = self.ang
         self.image.draw()
         
     def __str__(self):

src/Ships/Modules.py
@@ -45,6 +45,19 @@ class Module(Classes.Object, Classes.Vulnerable):
         ship.modules.append(self)
         return self
 
+    def set_rotation(self, deg):
+        super().set_rotation(deg)
+        self.place_ang = deg * np.pi / 180
+        rot_x = np.cos(self.place_ang) * self.radius
+        rot_y = np.sin(self.place_ang) * self.radius
+        self.placement = (rot_x, rot_y)
+        _x = self.ship.position[0] - rot_x
+        _y = self.ship.position[1] + rot_y
+        self.position = (_x, _y)
+        self.rect[0] = int(_x)
+        self.rect[1] = int(_y)
+
+    # the module position with respect to a center of the ship
     def rotate(self, deg):
         self.place_ang += deg*np.pi/180
         rot_x = np.cos(self.place_ang) * self.radius
@@ -163,11 +176,9 @@ class Weapon(Module):
                                       self.rect[1], self.range)
             shot.ang = self.ang
             shot.rect[0] = (self.rect[0]
-                                 - skipped_len * np.cos(np.deg2rad(shot.ang
-                                                                   + 90)))
+                            - skipped_len * np.cos(np.deg2rad(shot.ang + 90)))
             shot.rect[1] = (self.rect[1]
-                                 - skipped_len * np.sin(np.deg2rad(shot.ang
-                                                                   + 90)))
+                            - skipped_len * np.sin(np.deg2rad(shot.ang + 90)))
             shot.speed = [self.speed * np.cos(np.deg2rad(self.ang - 90)),
                           self.speed * np.sin(np.deg2rad(self.ang - 90))]
             shot.rotate(0)