0469ddbf24d599883c55fc25990b33d517e3f344
BobMak <vlmr.mko@gmail.com>
2022-07-28 18:12:09 -0700

    polygon surface generation wip

--- src/Ships/Shapes.py
+++ src/Ships/Shapes.py
@@ -1,13 +1,6 @@
-import math
-
 import numpy as np
 import random as r
 import copy
-
-import pygame as pg
-import pygame.transform as tr
-import pygame.image as img
-import pygame.gfxdraw as gfx
 
 from Core import Utils
 
@@ -36,10 +29,9 @@ class Point:
     def getDist(self, point):
         return np.sqrt((self.x-point.x)**2 + (self.y-point.y)**2)
 
-    def getClosest(self, points, allow_self=False):
-        if not allow_self:
-            if self in points:
-                points.remove(self)
+    def getClosest(self, points):
+        if self in points:
+            points.remove(self)
         points.sort(key=lambda a: self.getDist(a))
         return points
 
@@ -157,57 +149,49 @@ def generate_tree(size, dist=18, ddist=6, dang=60, branching=0.5, clusterRad=25,
     return points, cds, hull
 
 
-def shrink(hull: [[(Point, Point)]], n_points, symmetry=False):
+def shrink(hull: [[(Point, Point)]], a, symmetry=False):
     """given a hull with n layers and a parameter A, replace connections in
     outer layers with connections to inner layers.
     Replace a line in the outer layer with two lines going to an inner layer
     :param hull: set of layers"""
     outer = hull[0]
     inner = [l for subhull in hull[1:] for l in subhull]  # get lines from list of lists of lines
-    for idx in range(n_points):
-        try:
-            idx=idx*2
-            _outer_points = [p for l in outer for p in l]
-            _inner_points = [p for l in inner for p in l]
-            if idx >= len(outer):
-                break
-            p1, p2 = outer[idx]  # outer edge to be removed
+    for idx in range(a):
+        idx=idx*2
+        _outer_points = [p for l in outer for p in l]
+        _inner_points = [p for l in inner for p in l]
+        if idx >= len(outer):
+            break
+        p1, p2 = outer[idx]  # outer edge to be removed
 
-            # find closest line from the inner edge
-            new_p1 = p1.getClosest(_inner_points)[0]
-            _inner_points.remove(new_p1)
-            new_p2 = p2.getClosest(_inner_points)[0]
-            p1.color, new_p1.color = (255, 0, 0, 150), (255, 0, 0, 150)
-            p2.color, new_p2.color = (255, 0, 0, 150), (255, 0, 0, 150)
-            outer[idx] = (p1, new_p1)
-            outer.insert(idx+1, (p2, new_p2))
-            # outer.extend([, (p2, new_p2)])
-            # del outer[idx]
-            if symmetry:
-                # corresponding line on the other side
-                inv_p1 = Point(-p1.x, p1.y).getClosest(_outer_points, allow_self=True)[0]  # [x for line in outer for x in line]
-                _outer_points.remove(inv_p1)
-                inv_p2 = Point(-p2.x, p2.y).getClosest(_outer_points, allow_self=True)[0]  # get the inverse side of that
-                # if (inv_p2, inv_p1) in outer:
-                #     inv_idx = outer.index((inv_p2, inv_p1))
-                # elif (inv_p1, inv_p2) in outer:
-                #     inv_idx = outer.index((inv_p1, inv_p2))
-                # else:
-                #     print('no inverse point')
-                #     continue
-                inv_idx = -idx-1
-                # a new opposite side line in the inner layer
-                new_inv_p1 = Point(-new_p1.x, new_p1.y).getClosest(_inner_points)[0]
-                _inner_points.remove(new_inv_p1)
-                new_inv_p2 = Point(-new_p2.x, new_p2.y).getClosest(_inner_points)[0]
-                # del outer[inv_idx]
-                outer[inv_idx] = (inv_p1, new_inv_p1)
-                outer.insert(inv_idx, (inv_p2, new_inv_p2))
-                inv_p1.color, new_inv_p2.color = (255, 0, 0, 150), (255, 0, 0, 150)
-                inv_p2.color, new_inv_p2.color = (255, 0, 0, 150), (255, 0, 0, 150)
-                # outer.extend([(inv_p1, new_inv_p1), (inv_p2, new_inv_p2)])
-        except:
-            pass
+        # find closest line from the inner edge
+        # TODO: don't cut the line if it leaves a hanging point.
+        # TODO: cut shorter lines first.
+        new_p1 = p1.getClosest(_inner_points)[0]
+        _inner_points.remove(new_p1)
+        new_p2 = p2.getClosest(_inner_points)[0]
+        p1.color, new_p1.color = (255, 0, 0, 150), (255, 0, 0, 150)
+        p2.color, new_p2.color = (255, 0, 0, 150), (255, 0, 0, 150)
+        outer.extend([(p1, new_p1), (p2, new_p2)])
+        del outer[idx]
+        if symmetry:
+            # corresponding line on the other side
+            inv_p1 = Point(-p1.x, p1.y).getClosest(_outer_points)[0]  # [x for line in outer for x in line]
+            inv_p2 = Point(-p2.x, p2.y).getClosest(_outer_points)[0]  # get the inverse side of that
+            if (inv_p2, inv_p1) in outer:
+                inv_idx = outer.index((inv_p2, inv_p1))
+            elif (inv_p1, inv_p2) in outer:
+                inv_idx = outer.index((inv_p1, inv_p2))
+            else:
+                print('no inverse point')
+                continue
+            # a new opposite side line in the inner layer
+            new_inv_p1 = Point(-new_p1.x, new_p1.y).getClosest(_inner_points)[0]
+            new_inv_p2 = Point(-new_p2.x, new_p2.y).getClosest(_inner_points)[0]
+            del outer[inv_idx-1]
+            inv_p1.color, new_inv_p2.color = (255, 0, 0, 150), (255, 0, 0, 150)
+            inv_p2.color, new_inv_p2.color = (255, 0, 0, 150), (255, 0, 0, 150)
+            outer.extend([(inv_p1, new_inv_p1), (inv_p2, new_inv_p2)])
     return hull
 
 
@@ -215,7 +199,7 @@ def normal_points(center_point, n=5, std=5.0):
     return [Point(r.normalvariate(center_point[0], std), r.normalvariate(center_point[1], std)) for _ in range(n)]
 
 
-def generate_normals(center, npoints=5, std=5.0, nrecurs=1, dstd=1.0, leafhull=False, symmetric=False):
+def generate_normals(center, npoints=5, std=5.0, nrecurs=1, dstd=1.0, leafhull=False):
     """
     generates a few point clusters with random points scattered around those
     :param nclusers:
@@ -225,13 +209,11 @@ def generate_normals(center, npoints=5, std=5.0, nrecurs=1, dstd=1.0, leafhull=F
     """
     all_points = []
     points = normal_points(center, npoints, std)
-    if symmetric:
-        points = add_symmetry(points)
     hull = []
     if nrecurs > 0:
         for p in points:
             p.color = (255, 0, 0, 150)
-            _points, _hull = generate_normals(p, npoints, std-dstd, nrecurs-1, leafhull=leafhull, symmetric=symmetric)
+            _points, _hull = generate_normals(p, npoints, std-dstd, nrecurs-1, leafhull=leafhull)
             all_points.extend(_points)
             if leafhull:
                 hull.extend(_hull)
@@ -250,66 +232,3 @@ def generate_normals(center, npoints=5, std=5.0, nrecurs=1, dstd=1.0, leafhull=F
             if len(subHull)>2:
                 hull.append(subHull)
     return all_points, hull
-
-
-def generate_poly(size,
-                  temperature=0.5,
-                  sharpness=0.0,
-                  cuttosize=True):
-    surface = pg.Surface(size)
-    n_points = max(3, int((1.0-sharpness)*10))
-    n_recurs = max(1, int(min(n_points**2, temperature*3)))
-    n_verticies_to_remove = int(sharpness*(n_points-1))
-    leafhull = True if sharpness > 0.5 else False
-
-    green_blue_tmp = temperature**2 / math.exp(1)
-    clr = (int(temperature*255), int(green_blue_tmp*255), min(255,int((1.0-temperature)*255)), 100+int(temperature*100))
-
-    blocks, hull = generate_normals(
-        center=(0, 0),  # (screen.get_width()//2, screen.get_height()//2)
-        npoints=n_points,
-        nrecurs=n_recurs,
-        std=size[0]/10,
-        dstd=size[0]/20,
-        leafhull=leafhull,
-    )
-    surface.fill((0, 0, 0))
-    hull = shrink(hull, n_verticies_to_remove)
-    pg.event.pump()
-    # make sure all points fit inside the surface
-    # maxx = int(max([p.getX() for p in blocks]))
-    # maxy = int(max([p.getY() for p in blocks]))
-    #
-    # x_error_bias = maxx - size[0]
-    # y_error_bias = maxy - size[1]
-    # for p in blocks:
-    #     p.x -= x_error_bias
-    #     p.y -= y_error_bias
-    # minx = int(min([p.getX() for p in blocks]))
-    # miny = int(min([p.getY() for p in blocks]))
-    # scale_error = max(abs(minx/size[0]), abs(miny/size[1]))
-    # for p in blocks:
-    #     p.x /= scale_error
-    #     p.y /= scale_error
-
-    for subhull in hull:
-        gfx.filled_polygon(surface, [(p.getX(), p.getY()) for l in subhull for p in l], clr)
-
-    # if cuttosize:
-    #     surface = surface.subsurface(pg.Rect(minx, miny, maxx, maxy))
-    pg.display.flip()
-    return surface, blocks, hull
-
-
-def add_symmetry(points):
-    """
-    add symmetry to a set of points
-    :param points:
-    :return:
-    """
-    return points + [Point(-x, y) for x, y in points]
-
-
-def save_img(surface, filename, size=(100,100)):
-    surf = tr.scale(surface, size)
-    img.save(surf, filename)
--- src/generateShips.py
+++ src/generateShips.py
@@ -1,43 +1,33 @@
+import math
+import os
 import random
 import pygame as pg
 from pygame import gfxdraw as gfx
 import time
 
 from Ships import Generate
-
+from Ships.Generate import generate_poly
 
 if __name__ == "__main__":
     pg.init()
     screen = pg.display.set_mode((500, 500))
     # random.seed(145)
-    while 1:
-        symmetric = False # random.choice([True, False])  # symmetrical or asymmetrical
-        size = 100
-        radius = size*4
-        radius_diff = 5
-        ang = 0.5
-
-        # blocks, cds, hull = Generate.generate_tree(30, symmetry=False)
-        blocks, hull = Generate.generate_normals(
-            center=(0, 0),  # (screen.get_width()//2, screen.get_height()//2)
-            npoints=5,
-            nrecurs=2,
-            std=25.0,
-            dstd=7.0,
-            leafhull=True
-        )
-        screen.fill((0, 0, 0))
-        hull = Generate.shrink(hull, 5, symmetry=symmetric)
-        pg.event.pump()
-        # for b in blocks:
-        #     # pg.draw.rect(screen, b.color, (b.getX()-5, b.getY()-5, 10, 10))
-        #     pg.draw.circle(screen, b.color, (b.getX(), b.getY()), 2)
-        # for line in [l for subhull in hull for l in subhull]:
-        #     c = (255, 0,0, 150) if (255,0,0, 150)==line[0].color==line[1].color else (0,255,0, 150)
-        #     gfx.line(screen, int(line[0].getX()), int(line[0].getY()), int(line[1].getX()), int(line[1].getY()), c)
-        # fill polygon for the outermost hull
-        for subhull in hull:
-            gfx.filled_polygon(screen, [(p.getX(), p.getY()) for l in subhull for p in l], (0, 255, 0, 100))
-
-        pg.display.flip()
-        time.sleep(1)
+    samples = (1000, 100)
+    os.makedirs("imgs/train", exist_ok=True)
+    os.makedirs("imgs/test", exist_ok=True)
+    for folder, n_samples in zip(["imgs/train", "imgs/test"], samples):
+        labels = [0] * n_samples
+        for i in range(n_samples):
+            temperature = max(0.0, min(1.0, random.normalvariate(0.2, 0.3)))
+            sharpness = max(0.0, min(1.0, random.normalvariate(0.8, 0.3)))
+            labels[i] = round((temperature + sharpness) / 2)
+            img, _, _ = generate_poly(
+                (500,500),
+                temperature=temperature,
+                sharpness=sharpness
+            )
+            screen.blit(img, (0,0))
+            im_name = f'{folder}/{i}_tmp{temperature:.2f}-srp{sharpness:.2f}-{labels[i]}.png'
+            # Generate.save_img(screen, im_name, size=(100, 100))
+            pg.display.flip()
+            time.sleep(1.0)
