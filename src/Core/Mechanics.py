better vs realistic rotation wip; fixed shield angle misalignment.

src/Core/Mechanics.py
@@ -53,7 +53,7 @@ class Moving:
         self.a = acceleration
         self.aa = angular_acceleration
         # non-elastic collision
-        self.COLLISION_ELASTICITY = -0.2
+        self.COLLISION_ELASTICITY = 0.2
         # float representation of the position, since rect.x and rect.y are ints
         # we will lose a lot of precision when moving if we just update ints
         self.pos = (self.rect.x, self.rect.y)
@@ -491,7 +491,7 @@ class Animation(GObject):
     '''
     buffer = {}
     def __init__(self, images_arr, width, height, x, y, rand=False, finit=True,
-                type=0, hold_f=None, delay=0, state=None):
+                type=0, hold_f=None, delay=0, look_dir=-90, state=None):
         super().__init__(images_arr[0], x, y, width=width, height=height, state=state)
         self.frames_count = 0
         self.delay_count = 0
@@ -499,7 +499,7 @@ class Animation(GObject):
         if rand:
             self.look_dir = random.randint(-180, 180)
         else:
-            self.look_dir = -90
+            self.look_dir = look_dir
         self.frames = len(images_arr)
         self.type = type
         self.delay = delay
@@ -686,18 +686,20 @@ class Animation(GObject):
         return obj
 
     @staticmethod
-    def FX_engine_mark(source):
+    def FX_engine_mark(source, rotation=90, direction=0, dist=None, w=10, h=10):
+        if not dist:
+            dist = source.rect.height // 2
         object = Animation(
-            Assets.engi, 10, 10,
+            Assets.engi, w, h,
             source.rect.centerx
-               + source.rect.height // 2
-               * np.cos(np.deg2rad(source.look_dir + 90)),
+               + dist
+               * np.cos(np.deg2rad(source.look_dir + rotation)),
             source.rect.centery
-               + source.rect.height // 2
-               * np.sin(np.deg2rad(source.look_dir + 90)),
+               + dist
+               * np.sin(np.deg2rad(source.look_dir + rotation)),
             state=source.state
         )
-        object.look_dir = source.look_dir
+        object.look_dir = source.look_dir + direction
         object.v = source.v
 
     @staticmethod

src/Ships/Ship.py
@@ -1,3 +1,5 @@
+import time
+
 import Utils
 from Core.Mechanics import *
 
@@ -47,7 +49,7 @@ class Ship(GObject, Vulnerable):
 
         self.max_hp = hp
         self.max_shield_hp = shield
-        self.rotation_rate = rotation_rate // 2
+        self.rotation_rate = rotation_rate #// 2
         self.rotation_rate_max = rotation_rate
         self.acceleration = acceleration
         self.deacceleration = deacceleration
@@ -104,6 +106,8 @@ class Ship(GObject, Vulnerable):
         # stabilizers should be applied.
         self.target_rotation = self.look_dir
         self.isrotating = False
+        # don't allow the ship to accelerate more if it's momentum is too big
+        self.dostabilize = True
 
     def addMissiles(self, number):
         self.missiles += number
@@ -162,40 +166,52 @@ class Ship(GObject, Vulnerable):
     def control_rotate(self, dir):
         super()._rotate(self.rotation_rate*dir)
 
-    def rotate(self, ang, manual=True):
+    def rotate(self, ang, manual=True, pow=1.0):
         # super().rotate(ang)
-        self.rotation_rate = np.min([self.rotation_rate_max, self.rotation_rate + self.rotation_rate_max/300])
-        if manual:
-            self.isrotating = 5
-            self.target_rotation = self.look_dir + ang * self.rotation_rate
         sign = np.sign(ang)
-        super().apply_force_angular(self.rotation_rate*sign)
+        acc = True
+        if manual:
+            self.rotation_rate = np.min([self.rotation_rate_max, self.rotation_rate + self.rotation_rate_max / 300])
+            rotation_rate = self.rotation_rate
+            self.isrotating = True
+            self.target_rotation = self.look_dir #+ ang * self.rotation_rate_max
+            if self.dostabilize:
+                # don't acclererate in that direction if the angular momentum is too big
+                mdiff = self.av * self.m - self.rotation_rate_max * sign * 30
+                if mdiff * sign > 0:
+                    acc = False
+        else:
+            rotation_rate = self.rotation_rate_max * pow
+        if acc:
+            super().apply_force_angular(rotation_rate*sign)
+            # if self.rotation_rate_max > 0.1:
+            # 90+90*self.rect.width/self.rect.height
+            Animation.FX_engine_mark(self, 90 + 165*sign, direction=sign*90, h=int(self.rotation_rate_max), w=int(self.rotation_rate/2))
+
         for x in self.turrets:
-            x.apply_force_angular(self.rotation_rate)
-            Utils.orbit_rotate(self, x, -self.rotation_rate * sign,
+            # x.apply_force_angular(self.rotation_rate_max)
+            Utils.orbit_rotate(self, x, -self.rotation_rate_max * sign,
                                x.distance, x.orbit_ang)
 
         for x in self.shields:
-            x.apply_force_angular(self.rotation_rate*sign)
+            # x.apply_force_angular(self.rotation_rate_max*sign)
+            x._rotate(self.av)
 
         for x in self.hull_group:
-            Utils.orbit_rotate(self, x, -self.rotation_rate * sign,
+            Utils.orbit_rotate(self, x, -self.rotation_rate_max * sign,
                                x.distance, x.orbit_ang)
 
     def stabilize(self):
         """if the user is not pressing rotate, the ship should stick to the current
         rotation angle and position"""
-        diff_ang = self.target_rotation - self.look_dir
+        diff_ang = Utils.angle_diff(self.target_rotation, self.look_dir+self.av)
         if not self.isrotating and abs(diff_ang) > 0.01:
             if diff_ang > 180:
                 diff_ang -= 360
             if diff_ang < -180:
                 diff_ang += 360
-            diff_avel = self.av
-            super().apply_force_angular(
-                + np.min([np.abs(diff_ang), self.rotation_rate_max])*np.sign(diff_ang) \
-                - np.min([np.abs(diff_avel), self.rotation_rate_max])
-            )
+            pow = np.min([1.0, abs(diff_ang) / self.rotation_rate_max])
+            self.rotate(np.sign(diff_ang), manual=False, pow=pow)
 
     def draw_rotating(self):
         super().draw_rotating()
@@ -205,10 +221,11 @@ class Ship(GObject, Vulnerable):
     def update(self):
         self.acceleration_reserve = min(self.max_acceleration_reserve,
                                         self.acceleration_reserve_regeneration + self.acceleration_reserve)
-        if self.isrotating==0:
+        if not self.isrotating:
             self.stabilize()
-        else:
-            self.isrotating -= 1
+        self.isrotating = False
+        # else:
+        #     self.isrotating -= 1
 
         for n in range(len(self.locks)):
             if self.locks[n]: